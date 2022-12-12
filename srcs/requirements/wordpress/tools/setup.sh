b4b3d85870212ce7f42b231a20ed4c44597e3e51
ostef <47391841+ostef@users.noreply.github.com>
2022-12-12 19:56:40 +0100

    Wordpress: removed wp-cli.yml config file, use --path option in all of the wp invocations in setup.sh

--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -3,18 +3,18 @@
 echo "== Installing and setting up Wordpress =="
 
 # Download
-wp core download --allow-root
+wp core download --path=/var/www/html/wordpress --allow-root
 
 # Create config file wp-config.php with the appropriate database parameters (these are env variables in the .env file)
-wp config create --allow-root --dbname=$DB_DATABASE --dbhost=$DB_HOST --dbprefix=wp_ --dbuser=$DB_USER_NAME --dbpass=$DB_USER_PASSWORD
+wp config create --path=/var/www/html/wordpress --allow-root --dbname=$DB_DATABASE --dbhost=$DB_HOST --dbprefix=wp_ --dbuser=$DB_USER_NAME --dbpass=$DB_USER_PASSWORD
 
 # Install wordpress for our website (again, variables are in the .evn file)
-wp core install --allow-root --url=$DOMAIN_NAME --title=$WP_SITE_TITLE --admin_user=$WP_ADMIN_NAME --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL
+wp core install --path=/var/www/html/wordpress --allow-root --url=$DOMAIN_NAME --title=$WP_SITE_TITLE --admin_user=$WP_ADMIN_NAME --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL
 
-wp plugin update --allow-root --all
+wp plugin update --path=/var/www/html/wordpress --allow-root --all
 
 # Create default user
-wp user create --allow-root $WP_USER_NAME $WP_USER_EMAIL --user_pass=$WP_USER_PASSWORD
+wp user create --path=/var/www/html/wordpress --allow-root $WP_USER_NAME $WP_USER_EMAIL --user_pass=$WP_USER_PASSWORD
 
 # Set the owner of the content of our site to www-data user and group
 # For security reasons, we want to restrict who has access to these files
